9f2fc226b501761df0c6af262a269da1bb9ba70b
Jiajian Liang <13140979@student.uts.edu.au>
2019-05-31 02:14:29 +1000

    merge with sakib login
    issue: Login not working -> need access sakib's computer

---
+++ DoctorsPoint/DoctorsPoint/ViewController/LoginVC.swift
@@ -0,0 +1,63 @@
+
+import UIKit
+import Firebase
+
+class LoginVC: UIViewController {
+    
+//    let myString = StringCollection()
+//    
+//    @IBOutlet weak var logoTopConstraintHeight: NSLayoutConstraint!
+//    @IBOutlet weak var topConstraintHeight: NSLayoutConstraint!
+//    
+//    @IBAction func ShowSignInPop(_ sender: Any) {
+//        topConstraintHeight.constant = 100
+//        logoTopConstraintHeight.constant = 100
+//        UIView.animate(withDuration: 0.2, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0, options: .curveEaseOut, animations: {self.view.layoutIfNeeded()}, completion: nil)
+//        
+//        print("clicked")
+//    }
+//    
+//    //    @IBAction func hideSignInPop(_ sender: Any) {
+//    //        topConstraintHeight.constant = 800
+//    //        logoTopConstraintHeight.constant = 238
+//    //        UIView.animate(withDuration: 0.2, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0, options: .curveEaseOut, animations: {self.view.layoutIfNeeded()}, completion: nil)
+//    //        print("clicked")
+//    //    }
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        topConstraintHeight.constant = 200
+//        logoTopConstraintHeight.constant = 238
+//        
+//        
+//    }
+//    
+//    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(animated)
+//    }
+//    
+//    
+//    func checkLogined() {
+//        if Auth.auth().currentUser != nil
+//        {
+//            print("Current User valid \(Auth.auth().currentUser!.uid)")
+//            goToPatientMenu()
+//        }
+//        else
+//        {
+//            print("No Current User")
+//        }
+//        
+//    }
+//    
+//    
+//    
+//    func goToPatientMenu() {
+//        performSegue(withIdentifier: myString.toPatientMainView, sender: self)
+//    }
+//    
+//    func goToDoctorMenu() {
+//        performSegue(withIdentifier: myString.toDoctorMainView, sender: self)
+//    }
+    
+    
+}
